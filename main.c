251ba6c5f979dae1556b897e8ee18d5959505006
absaGH <absa3852@gmail.com>
2021-05-12 14:24:49 +0000

    montyinterpreter

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * main - the starting point of execution of the program
+ *
+ * @argc: the number of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ *
+ * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ */
+int main(int argc, char **argv)
+{
+	FILE *monty_file = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
+	monty_file = fopen(argv[1], "r");
+	if (monty_file == NULL)
+	{
+		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	execute_monty(monty_file);
+	fclose(monty_file);
+	return (EXIT_SUCCESS);
+}
